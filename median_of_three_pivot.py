894e495669ca97c5ec73600d7d56b6e7223dd8f7
Tanvir Tazul Islam <tanvirtazul@gmail.com>
2025-11-12 15:40:46 +1030

    Implement alternate solution for median of three pivot
    Added an alternate solution for finding the median of three pivot elements.

--- median_of_three_pivot.py
+++ median_of_three_pivot.py
@@ -2,8 +2,7 @@ def median_of_three_pivot(arr, low, high):
     if len(arr) == 0:
         return None
     
-    mid = (high + low)//2
-    # print(arr[mid])
+    mid = (high + low) // 2
     
     if arr[low] > arr[mid]:
         if arr[low] > arr[high]:
@@ -33,8 +32,25 @@ def median_of_three_pivot(arr, low, high):
             minimum = low
     
     return arr[median]
+
+
+## alternate solution
+def median_of_three_pivot_alternate_solution(arr, low, high):
+    if len(arr) == 0:
+        return None
+    
+    mid = (high + low) // 2
+    # print(arr[mid])
+
+    if arr[low] < arr[mid] < arr[high] or arr[high] < arr[mid] < arr[low]:
+        return arr[mid]
+    elif arr[mid] < arr[low] < arr[high] or arr[high] < arr[low] < arr[mid]:
+        return arr[low]
+    else:
+        return arr[high]    
         
             
 
 # median_of_three_pivot([10, 9, 5, 2, 6, 4, 7], 0, 6)
-median_of_three_pivot([10, 9, 5, 2, 6, 4, 7], 0, 6)
+print(median_of_three_pivot([10, 9, 5, 2, 6, 4, 7], 0, 6))
+print(median_of_three_pivot_alternate_solution([10, 9, 5, 2, 6, 4, 7], 0, 6))
